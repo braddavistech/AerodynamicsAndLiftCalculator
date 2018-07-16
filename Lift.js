5a9f64034bfde8421713299f980fee48172b7f0a
Brad Davis <brad@braddavistech.com>
2018-07-15 21:18:30 -0500

    This is a first round of a basic html file with a js file for the Aerospace Calculator application.
    I worked on this code using Atom editor and uploaded my progress. I have set up an array to store planes as objects to recall different calculations at different times. It also has a function created to allow for quicker display on screen. This will move into taking different measurements (metric/standard) and be able to solve for other variables.

---
+++ Lift.js
@@ -0,0 +1,61 @@
+var cLift
+var density
+var velocity
+var wingArea
+var lift
+var differentPlanes
+var counter = 1;
+var arrayCounter = 0;
+
+function print (listObject){
+  document.write('The coefficient of lift for Aircraft # ' + counter + ' is ' + cLift + '.<br>');
+  document.write('The density around Aircraft # ' + counter + ' is ' + density + ' kilograms per cubic meter.<br>');
+  document.write('The velocity of lift for Aircraft # ' + counter + ' is ' + velocity + 'meters per second.<br>');
+  document.write('The wing area for Aircraft # ' + counter + ' is ' + wingArea + ' square meters.<br>');
+  document.write('The force of lift for Aircraft # ' + counter + ' is ' + lift + ' Newtons.<br>');
+  document.write('This is in differentPlanes[' + arrayCounter + ']. <br><br>');
+}
+
+cLift = parseFloat(prompt('What is the coefficient of lift for aircraft 1?'));
+density = parseFloat(prompt('What is the air density for aircraft 1 in kilograms per cubic meter?'));
+velocity = parseFloat(prompt('What is the velocity for aircraft 1 in meters per second'));
+wingArea = parseFloat(prompt('What is the wing area for aircraft 1 in square meters?'));
+lift = .5 * cLift * density * velocity * velocity * wingArea;
+
+var plane = {
+  cLift,
+  density,
+  velocity,
+  wingArea,
+  lift
+}
+
+differentPlanes = [plane];
+
+print(plane);
+
+var nextPlane = prompt('Would you like to enter another equation? (Enter Yes or No)');
+nextPlane = nextPlane.toUpperCase();
+
+while (nextPlane != 'NO') {
+    counter += 1;
+    arrayCounter += 1;
+    cLift = parseFloat(prompt('What is the coefficient of lift for aircraft ' + counter + ' ?'));
+    density = parseFloat(prompt('What is the air density for aircraft ' + counter + ' in kilograms per cubic meter?'));
+    velocity = parseFloat(prompt('What is the velocity for aircraft ' + counter + ' in meters per second'));
+    wingArea = parseFloat(prompt('What is the wing area for aircraft ' + counter + ' in square meters?'));
+    lift = .5 * cLift * density * velocity * velocity * wingArea;
+
+    var plane = {
+        cLift,
+        density,
+        velocity,
+        wingArea,
+        lift
+      }
+
+    differentPlanes.push(plane);
+    print(plane);
+    nextPlane = prompt('Would you like to enter another equation? (Enter Yes or No)');
+    nextPlane = nextPlane.toUpperCase();
+}
