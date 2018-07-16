baa9890a00a3a9efcd41bdfb17f29a035477c9d4
Brad Davis <brad@braddavistech.com>
2018-07-16 17:37:31 -0500

    Update Lift.js

--- Lift.js
+++ Lift.js
@@ -33,7 +33,7 @@ while (nextPlane == 'YES') {
         lift
       }
 
-    differentPlanes.push(plane);
+    differentPlanes.push[plane];
     print(plane);
     nextPlane = prompt('Would you like to enter another equation? (Enter Yes or No)');
     nextPlane = nextPlane.toUpperCase();
